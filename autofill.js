6df1b6cdd1d87ee139a2135dd340e9cfd3535763
Mattwmaster58 <mattwmaster58@gmail.com>
2023-04-07 04:33:03 -0600

    more bitwarden stuff

--- autofill.js
+++ autofill.js
@@ -18,21 +18,18 @@ function doFocusElement(el, setValue) {
 
 /**
  * Normalize the event based on API support
- * @param {HTMLElement} el
  * @param {string} eventName
  * @returns {Event} A normalized event
  */
-function normalizeEvent(el, eventName) {
-    var ev;
+function createKeyboardEvent(eventName) {
     if (!('KeyboardEvent' in window)) {
-        L.error("failed to create KeyboardEvent because the browser doesn't appear to support it");
+        L.error("failed to create KeyboardEvent because the browser doesn't appear to support it!");
+        return null;
     }
-    ev = new window.KeyboardEvent(eventName, {
+    return new window.KeyboardEvent(eventName, {
         bubbles: true,
         cancelable: false,
     });
-
-    return ev;
 }
 
 /**
@@ -44,9 +41,9 @@ function setValueForElement(el) {
     const valueToSet = el.value;
     clickElement(el);
     doFocusElement(el, false);
-    el.dispatchEvent(normalizeEvent(el, 'keydown'));
-    el.dispatchEvent(normalizeEvent(el, 'keypress'));
-    el.dispatchEvent(normalizeEvent(el, 'keyup'));
+    el.dispatchEvent(createKeyboardEvent('keydown'));
+    el.dispatchEvent(createKeyboardEvent('keypress'));
+    el.dispatchEvent(createKeyboardEvent('keyup'));
     el.value !== valueToSet && (el.value = valueToSet);
 }
 
@@ -60,9 +57,9 @@ function setValueForElementByEvent(el) {
         ev1 = el.ownerDocument.createEvent('HTMLEvents'),
         ev2 = el.ownerDocument.createEvent('HTMLEvents');
 
-    el.dispatchEvent(normalizeEvent(el, 'keydown'));
-    el.dispatchEvent(normalizeEvent(el, 'keypress'));
-    el.dispatchEvent(normalizeEvent(el, 'keyup'));
+    el.dispatchEvent(createKeyboardEvent('keydown'));
+    el.dispatchEvent(createKeyboardEvent('keypress'));
+    el.dispatchEvent(createKeyboardEvent('keyup'));
     ev2.initEvent('input', true, true);
     el.dispatchEvent(ev2);
     ev1.initEvent('change', true, true);
@@ -82,4 +79,58 @@ function clickElement(el) {
     }
     el.click();
     return true;
+}
+
+/**
+ * Determine if we can apply styling to `el` to indicate that it was filled.
+ * @param {HTMLElement} el
+ * @returns {boolean} Returns true if we can see the element to apply styling.
+ */
+function canSeeElementToStyle(el) {
+    var currentEl;
+    if (currentEl = animateTheFilling) {
+        a: {
+            currentEl = el;
+            for (var owner = el.ownerDocument, owner = owner ? owner.defaultView : {}, theStyle; currentEl && currentEl !== document;) {
+                theStyle = owner.getComputedStyle ? owner.getComputedStyle(currentEl, null) : currentEl.style;
+                if (!theStyle) {
+                    currentEl = true;
+                    break a;
+                }
+                if ('none' === theStyle.display || 'hidden' == theStyle.visibility) {
+                    currentEl = false;
+                    break a;
+                }
+                currentEl = currentEl.parentNode;
+            }
+            currentEl = currentEl === document;
+        }
+    }
+    // START MODIFICATION
+    if (el && !el.type && el.tagName.toLowerCase() === 'span') {
+        return true;
+    }
+    // END MODIFICATION
+    if (currentEl) {
+      const inputTypes = ['email', 'text', 'password', 'number', 'tel', 'url'];
+      return inputTypes.includes(el.type || '');
+    } else {
+      return false;
+    }
+}
+
+
+function doAllFillOperations(el, afterValSetFunc) {
+    setValueForElement(el);
+    afterValSetFunc(el);
+    setValueForElementByEvent(el);
+
+    // START MODIFICATION
+    if (canSeeElementToStyle(el)) {
+        el.classList.add('com-m4w2fa-browser-animated-fill');
+        setTimeout(function () {
+            if (el) el.classList.remove('com-m4w2fa-browser-animated-fill');
+        }, styleTimeout);
+    }
+    // END MODIFICATION
 }
